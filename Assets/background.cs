4de43252f3300fdf1a660e731ef3fa1809983ad2
Patrick Manhatton <macklehatton@gmail.com>
2013-12-19 14:27:47 -0600

    Started character generation

---
+++ Assets/background.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class background : MonoBehaviour {	
+	
+	// Modifiers	
+	
+	public int vitalityMod;	
+	public int agilityMod;	
+	public int wisdomMod;	
+	
+	public int atheleticsMod;	
+	public int acrobaticsMod;	
+	public int coordinationMod;	
+	public int perceptionMod;	
+	public int charismaMod;
+
+	
+	public void characterBackground(string backgroundName)		
+	{		
+		switch(backgroundName)			
+		{			
+		case "raider":			
+			vitalityMod = 1;			
+			agilityMod = 1;			
+			wisdomMod = 1;			
+			break;
+			
+		case "assassin":			
+			atheleticsMod = 1;			
+			acrobaticsMod = 2;			
+			break;			
+			
+		default:			
+			this.name = "Citizen";			
+			break;			
+		}		
+	}	
+}
---
+++ Assets/playerMain.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMain : MonoBehaviour {
+	
+	// Attributes
+	
+	public int vitality;
+	public int agility;
+	public int wisdom;
+	
+	public int athletics;
+	public int acrobatics;
+	public int coordination;
+	public int perception;
+	public int charisma;	
+	
+	
+	// Background
+	
+	public background Background;
+	
+	// Skills
+	
+	
+	
+	// Stats
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		Background.characterBackground("raider");
+		athletics += Background.atheleticsMod;
+		acrobatics += Background.acrobaticsMod;
+		
+	}
+	
+	void OnGUI () {
+		if (GUI.Button(new Rect(10, 70, 30, 30), "+"))
+			athletics += 1;
+		if (GUI.Button(new Rect(50, 70, 30, 30), "-"))
+			athletics -= 1;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+}
